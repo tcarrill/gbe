88d85aea6033653e287ac6973201180c1b99910e
tcarrill <thomas.carrill@gmail.com>
2019-04-26 14:31:30 -0400

    Implement more instructions from boot rom.

--- gbe.c
+++ gbe.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include "gbe.h"
 
+#define MASK8(x) (x & 0xF)
+
 const char *byte_to_binary(int x) {
     static char b[9];
     b[0] = '\0';
@@ -35,7 +37,20 @@ uint8_t fetch(Vm *vm) {
 	return opcode;
 }
 
-void bit(Vm *vm) {
+void rl_carry(uint8_t f, uint8_t r) {
+  	uint8_t out = BIT_CHECK(r, 7);
+	uint8_t in = BIT_CHECK(f, C);
+	out == 1 ? BIT_SET(f, C) : BIT_CLEAR(f, C);
+	r <<= 1;
+	if (in == 1) {
+		BIT_SET(r, 0);
+	} 
+	r == 0 ? BIT_SET(f, Z) : BIT_CLEAR(f, Z);
+	BIT_CLEAR(f, N);
+	BIT_CLEAR(f, H);
+}
+
+void cb(Vm *vm) {
 	Cpu *cpu = vm->cpu;
 	uint8_t opcode = fetch(vm);
 	switch (opcode) {
@@ -44,8 +59,17 @@ void bit(Vm *vm) {
 					BIT_CLEAR(cpu->f, N);
 					BIT_SET(cpu->f, H); 
 					break;
-					
+		case 0x11: 
+					rl_carry(cpu->f, cpu->c);
+					break;
 	}
+}
+
+void dec(uint8_t f, uint8_t r) {
+	r--;
+	r == 0 ? BIT_SET(f, Z) : BIT_CLEAR(f, Z);
+	MASK8(r) == 0xF ? BIT_SET(f, H) : BIT_CLEAR(f, H);
+	BIT_SET(f, N);
 }
 
 int emulate(Vm *vm) {
@@ -57,8 +81,15 @@ int emulate(Vm *vm) {
 	  case 0x02: todo_implement(vm); break;
 	  case 0x03: todo_implement(vm); break;
 	  case 0x04: todo_implement(vm); break;
-	  case 0x05: todo_implement(vm); break;
-	  case 0x06: todo_implement(vm); break;
+	  case 0x05:
+	  			cpu->b--;
+  	  			cpu->b == 0 ? BIT_SET(cpu->f, Z) : BIT_CLEAR(cpu->f, Z);
+  				MASK8(cpu->b) == 0xF ? BIT_SET(cpu->f, H) : BIT_CLEAR(cpu->f, H);
+  				BIT_SET(cpu->f, N);
+	  	 		break;
+	  case 0x06:
+	  			cpu->b = fetch(vm);
+	  			break;
 	  case 0x07: todo_implement(vm); break;
 	  case 0x08: todo_implement(vm); break;
 	  case 0x09: todo_implement(vm); break;
@@ -66,6 +97,9 @@ int emulate(Vm *vm) {
 	  case 0x0B: todo_implement(vm); break;
 	  case 0x0C: 
 	  			cpu->c++;
+				cpu->c == 0 ? BIT_SET(cpu->f, Z) : BIT_CLEAR(cpu->f, Z);
+				MASK8(cpu->c) == 0 ? BIT_SET(cpu->f, H) : BIT_CLEAR(cpu->f, H);
+				BIT_CLEAR(cpu->f, N);			
 				break;
 	  case 0x0D: todo_implement(vm); break;
 	  case 0x0E:
@@ -78,11 +112,22 @@ int emulate(Vm *vm) {
 				cpu->d = fetch(vm);
 				break;
 	  case 0x12: todo_implement(vm); break;
-	  case 0x13: todo_implement(vm); break;
+	  case 0x13: 
+				{
+					uint16_t de = (cpu->d << 8) | cpu->e;
+	 				de++;
+	 				cpu->d = de >> 8;
+	 				cpu->e = de & 0xFF;
+	 				break;
+ 				}
 	  case 0x14: todo_implement(vm); break;
 	  case 0x15: todo_implement(vm); break;
 	  case 0x16: todo_implement(vm); break;
-	  case 0x17: todo_implement(vm); break;
+	  case 0x17:
+	  			{
+	  		  	rl_carry(cpu->f, cpu->a);
+	  	 		break;
+				}
 	  case 0x18: todo_implement(vm); break;
 	  case 0x19: todo_implement(vm); break;
 	  case 0x1A: 
@@ -110,8 +155,23 @@ int emulate(Vm *vm) {
 	  			cpu->l = fetch(vm);
 	  		  	cpu->h = fetch(vm);
 				break;
-	  case 0x22: todo_implement(vm); break;
-	  case 0x23: todo_implement(vm); break;
+	  case 0x22: 
+				{
+					uint16_t address = (cpu->h << 8) | cpu->l;
+	 				vm->memory[address] = cpu->a;
+	 			   	address++;
+	 			  	cpu->h = address >> 8;
+	 			 	cpu->l = address & 0xFF;
+ 				}
+				break;
+	  case 0x23:
+	  			{
+					uint16_t hl = (cpu->h << 8) | cpu->l;
+   					hl++;
+  					cpu->h = hl >> 8;
+ 					cpu->l = hl & 0xFF;
+				}
+	  			break;
 	  case 0x24: todo_implement(vm); break;
 	  case 0x25: todo_implement(vm); break;
 	  case 0x26: todo_implement(vm); break;
@@ -151,57 +211,62 @@ int emulate(Vm *vm) {
 	  case 0x3A: todo_implement(vm); break;
 	  case 0x3B: todo_implement(vm); break;
 	  case 0x3C: todo_implement(vm); break;
-	  case 0x3D: todo_implement(vm); break;
+	  case 0x3D:
+				cpu->a--;
+				cpu->a == 0 ? BIT_SET(cpu->f, Z) : BIT_CLEAR(cpu->f, Z);
+				MASK8(cpu->a) == 0xF ? BIT_SET(cpu->f, H) : BIT_CLEAR(cpu->f, H);
+				BIT_SET(cpu->f, N);
+				break;
 	  case 0x3E: 
 	  			cpu->a = fetch(vm);
 				break;
 	  case 0x3F: todo_implement(vm); break;
-	  case 0x40: todo_implement(vm); break;
-	  case 0x41: todo_implement(vm); break;
-	  case 0x42: todo_implement(vm); break;
-	  case 0x43: todo_implement(vm); break;
-	  case 0x44: todo_implement(vm); break;
-	  case 0x45: todo_implement(vm); break;
+	  case 0x40: cpu->b = cpu->b; break;
+	  case 0x41: cpu->b = cpu->c; break;
+	  case 0x42: cpu->b = cpu->d; break;
+	  case 0x43: cpu->b = cpu->e; break;
+	  case 0x44: cpu->b = cpu->h; break;
+	  case 0x45: cpu->b = cpu->l; break;
 	  case 0x46: todo_implement(vm); break;
 	  case 0x47: todo_implement(vm); break;
-	  case 0x48: todo_implement(vm); break;
-	  case 0x49: todo_implement(vm); break;
-	  case 0x4A: todo_implement(vm); break;
-	  case 0x4B: todo_implement(vm); break;
-	  case 0x4C: todo_implement(vm); break;
-	  case 0x4D: todo_implement(vm); break;
+	  case 0x48: cpu->c = cpu->b; break;
+	  case 0x49: cpu->c = cpu->c; break;
+	  case 0x4A: cpu->c = cpu->d; break;
+	  case 0x4B: cpu->c = cpu->e; break;
+	  case 0x4C: cpu->c = cpu->h; break;
+	  case 0x4D: cpu->c = cpu->l; break;
 	  case 0x4E: todo_implement(vm); break;
-	  case 0x4F: todo_implement(vm); break;
-	  case 0x50: todo_implement(vm); break;
-	  case 0x51: todo_implement(vm); break;
-	  case 0x52: todo_implement(vm); break;
-	  case 0x53: todo_implement(vm); break;
-	  case 0x54: todo_implement(vm); break;
-	  case 0x55: todo_implement(vm); break;
+	  case 0x4F: cpu->c = cpu->a; break;
+	  case 0x50: cpu->d = cpu->b; break;
+	  case 0x51: cpu->d = cpu->c; break;
+	  case 0x52: cpu->d = cpu->d; break;
+	  case 0x53: cpu->d = cpu->e; break;
+	  case 0x54: cpu->d = cpu->h; break;
+	  case 0x55: cpu->d = cpu->l; break;
 	  case 0x56: todo_implement(vm); break;
 	  case 0x57: todo_implement(vm); break;
-	  case 0x58: todo_implement(vm); break;
-	  case 0x59: todo_implement(vm); break;
-	  case 0x5A: todo_implement(vm); break;
-	  case 0x5B: todo_implement(vm); break;
-	  case 0x5C: todo_implement(vm); break;
-	  case 0x5D: todo_implement(vm); break;
+	  case 0x58: cpu->e = cpu->b; break;
+	  case 0x59: cpu->e = cpu->c; break;
+	  case 0x5A: cpu->e = cpu->d; break;
+	  case 0x5B: cpu->e = cpu->e; break;
+	  case 0x5C: cpu->e = cpu->h; break;
+	  case 0x5D: cpu->e = cpu->l; break;
 	  case 0x5E: todo_implement(vm); break;
 	  case 0x5F: todo_implement(vm); break;
-	  case 0x60: todo_implement(vm); break;
-	  case 0x61: todo_implement(vm); break;
-	  case 0x62: todo_implement(vm); break;
-	  case 0x63: todo_implement(vm); break;
-	  case 0x64: todo_implement(vm); break;
-	  case 0x65: todo_implement(vm); break;
+	  case 0x60: cpu->h = cpu->b; break;
+	  case 0x61: cpu->h = cpu->c; break;
+	  case 0x62: cpu->h = cpu->d; break;
+	  case 0x63: cpu->h = cpu->e; break;
+	  case 0x64: cpu->h = cpu->h; break;
+	  case 0x65: cpu->h = cpu->l; break;
 	  case 0x66: todo_implement(vm); break;
 	  case 0x67: todo_implement(vm); break;
-	  case 0x68: todo_implement(vm); break;
-	  case 0x69: todo_implement(vm); break;
-	  case 0x6A: todo_implement(vm); break;
-	  case 0x6B: todo_implement(vm); break;
-	  case 0x6C: todo_implement(vm); break;
-	  case 0x6D: todo_implement(vm); break;
+	  case 0x68: cpu->l = cpu->b; break;
+	  case 0x69: cpu->l = cpu->c; break;
+	  case 0x6A: cpu->l = cpu->d; break;
+	  case 0x6B: cpu->l = cpu->e; break;
+	  case 0x6C: cpu->l = cpu->h; break;
+	  case 0x6D: cpu->l = cpu->l; break;
 	  case 0x6E: todo_implement(vm); break;
 	  case 0x6F: todo_implement(vm); break;
 	  case 0x70: todo_implement(vm); break;
@@ -217,14 +282,14 @@ int emulate(Vm *vm) {
 	 				vm->memory[address] = cpu->a;
 	 				break;
  				}
-	  case 0x78: todo_implement(vm); break;
-	  case 0x79: todo_implement(vm); break;
-	  case 0x7A: todo_implement(vm); break;
-	  case 0x7B: todo_implement(vm); break;
-	  case 0x7C: todo_implement(vm); break;
-	  case 0x7D: todo_implement(vm); break;
+	  case 0x78: cpu->a = cpu->b; break;
+	  case 0x79: cpu->a = cpu->c; break;
+	  case 0x7A: cpu->a = cpu->d; break;
+	  case 0x7B: cpu->a = cpu->e; break;
+	  case 0x7C: cpu->a = cpu->h; break;
+	  case 0x7D: cpu->a = cpu->l; break;
 	  case 0x7E: todo_implement(vm); break;
-	  case 0x7F: todo_implement(vm); break;
+	  case 0x7F: cpu->a = cpu->a; break;
 	  case 0x80: todo_implement(vm); break;
 	  case 0x81: todo_implement(vm); break;
 	  case 0x82: todo_implement(vm); break;
@@ -274,7 +339,7 @@ int emulate(Vm *vm) {
 	  case 0xAE: todo_implement(vm); break;
 	  case 0xAF: 
 	  			cpu->a ^= cpu->a;
-	            cpu->f |= (cpu->a == 0) << Z;
+				cpu->a == 0 ? BIT_SET(cpu->f, Z) : BIT_CLEAR(cpu->f, Z);
 				BIT_CLEAR(cpu->f, N);
 				BIT_CLEAR(cpu->f, H);
 				BIT_CLEAR(cpu->f, C);
@@ -296,7 +361,12 @@ int emulate(Vm *vm) {
 	  case 0xBE: todo_implement(vm); break;
 	  case 0xBF: todo_implement(vm); break;
 	  case 0xC0: todo_implement(vm); break;
-	  case 0xC1: todo_implement(vm); break;
+	  case 0xC1:
+	  			cpu->c = vm->memory[cpu->sp];
+	  		  	cpu->sp++;
+	  		  	cpu->b = vm->memory[cpu->sp];
+	  		  	cpu->sp++;
+	  			break;
 	  case 0xC2: todo_implement(vm); break;
 	  case 0xC3: 
 	  			{
@@ -306,17 +376,35 @@ int emulate(Vm *vm) {
 					break;
 				}
 	  case 0xC4: todo_implement(vm); break;
-	  case 0xC5: todo_implement(vm); break;
+	  case 0xC5:
+	  			cpu->sp--;
+	  		  	vm->memory[cpu->sp] = cpu->b;				
+	  		  	cpu->sp--;
+	  		  	vm->memory[cpu->sp] = cpu->c;
+	  			break;
 	  case 0xC6: todo_implement(vm); break;
 	  case 0xC7: todo_implement(vm); break;
 	  case 0xC8: todo_implement(vm); break;
-	  case 0xC9: todo_implement(vm); break;
+	  case 0xC9:
+	  			cpu->pc = vm->memory[cpu->sp] | (vm->memory[cpu->sp + 1] << 8);
+				cpu->sp += 2;
+	  			break;
 	  case 0xCA: todo_implement(vm); break;
 	  case 0xCB: 
-	  			bit(vm);
+	  			cb(vm);
 				break;
 	  case 0xCC: todo_implement(vm); break;
-	  case 0xCD: todo_implement(vm); break;
+	  case 0xCD: 
+	  			{
+	  				uint16_t ret = cpu->pc + 2;
+	  				vm->memory[cpu->sp - 1] = (ret >> 8) & 0xff;
+	  				vm->memory[cpu->sp - 2] = ret & 0xff;
+	  				cpu->sp = cpu->sp - 2;
+	  				uint8_t lb = fetch(vm);
+	  		  		uint8_t hb = fetch(vm); 
+	  				cpu->pc = (hb << 8) | lb;		
+	  			}
+	   			break;
 	  case 0xCE: todo_implement(vm); break;
 	  case 0xCF: todo_implement(vm); break;
 	  case 0xD0: todo_implement(vm); break;
@@ -356,7 +444,12 @@ int emulate(Vm *vm) {
 	  case 0xE7: todo_implement(vm); break;
 	  case 0xE8: todo_implement(vm); break;
 	  case 0xE9: todo_implement(vm); break;
-	  case 0xEA: todo_implement(vm); break;
+	  case 0xEA:
+	  			{
+					uint8_t address = (fetch(vm) << 8) | fetch(vm);
+	  		  		vm->memory[address] = cpu->a;
+	  				break;
+				}
 	  case 0xEB: todo_implement(vm); break;
 	  case 0xEC: todo_implement(vm); break;
 	  case 0xED: todo_implement(vm); break;
@@ -376,7 +469,16 @@ int emulate(Vm *vm) {
 	  case 0xFB: todo_implement(vm); break;
 	  case 0xFC: todo_implement(vm); break;
 	  case 0xFD: todo_implement(vm); break;
-	  case 0xFE: todo_implement(vm); break;
+	  case 0xFE:  
+	  {
+		  uint8_t value = fetch(vm);
+		  uint8_t res = cpu->a - value;
+		  res == 0 ? BIT_SET(cpu->f, Z) : BIT_CLEAR(cpu->f, Z);
+		  cpu->a < res ? BIT_SET(cpu->f, C) : BIT_CLEAR(cpu->f, C);
+		  (MASK8(cpu->a) - MASK8(value)) < 0 ? BIT_SET(cpu->f, H) : BIT_CLEAR(cpu->f, H);
+		  BIT_SET(cpu->f, N);
+	  }
+	  break;
 	  case 0xFF: todo_implement(vm); break;   
   	}
 
